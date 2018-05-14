c5a47ff25fc82ba9b3342dbcec2f21141ad96a0d
YonggeLi <limipid.eye@gmail.com>
2018-05-14 23:10:51 +0800

    for test

--- conv_net_3layers.py
+++ conv_net_3layers.py
@@ -283,8 +283,9 @@ class Model(object):
 
     def load(self):
         print('load model:', FLAGS.my_best_model)
-        self.saver.restore(self.sess, FLAGS.my_best_model)
-
+        self.saver = tf.train.import_meta_graph(FLAGS.my_best_model)
+        self.saver.restore(self.sess, tf.train.latest_checkpoint('./'))
+        
 #################################
 
 ################ define train and test #################
